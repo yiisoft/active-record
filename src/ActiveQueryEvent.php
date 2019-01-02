ff896d72fd01a3047f0e4293b8d13e4448032506
Fabrizio Caldarelli <fabrizio.caldarelli@gmail.com>
2019-01-02 23:48:31 +0100

    Fix events and tests
    * Fixed ActiveQuery tests
    
    * refactoring
    
    * Missing models to fix tests
    
    * Removed unuseful init() method in Animal test file
    
    * Added yii\di\Initialble to ActiveQuery;
    Remove duplicated yii\activerecord\AfterSaveEvent ( there is yii\db\AfterSaveEvent )
    Adjusted CustomerQuery test data after implemented yii\di\Initiable ActiveQuery
    
    * Replaced ModelEvent with new ValidationEvent inside BaseActiveRecord
    
    * Removed unuseful constructor parameter
    
    * ActiveRecordSaveEvent to manage before ad after save() ActiveRecord event;
    Updated beforeSave() and afterSave() ActiveRecord content;
    Updated ActiveRecordTests where model field were filled inside the constructor;
    Updated namespace reference inside ActiveRecordTestTrait;
    
    * typo in ActiveRecordSaveEvent
    
    * Added ActiveQueryEvent;
    Fixed ActiveQueryTests;
    
    * Added ActiveRecordDelete event;
    Added ActiveRecordFind event;
    
    * fixed ActiveRecordTest;
    Added ActiveRecordRefresh event;
    
    * Added ActiveRecordInitEvent;
    Refactoring on BaseActiveRecord events reference names;
    
    * Updated construct in ActiveQuery to complete the configuration using AbstractContainer::configure();
    Updated ActiveQueryEvent::INIT constant value;
    Updated ActiveRecordSave constant values;
    Refactoring. Removed ActiveRecordDeleteEvent because replaced by ActiveRecordEvent;
    Refactoring. Removed ActiveRecordFindEvent because replaced by ActiveRecordEvent;
    Refactoring. Removed ActiveRecordRefreshEvent because replaced by ActiveRecordEvent;
    Refactoring. Removed ActiveRecordInitEvent because replaced by ActiveRecordEvent;
    Refactoring. Created ActiveRecordEvent class that manage init, find, refresh and delete event types;
    Updated BaseActiveRecord trigger first parameter;
    Replace in Event::on and ::off all first string parameter with ::class reference;
    
    * type hinting

---
+++ src/ActiveQueryEvent.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\activerecord;
+
+use yii\base\Event;
+
+
+/**
+ * ActiveQueryEvent represents the event parameter used for an active queryevent.
+ *
+ * @author Fabrizio Caldarelli <fabrizio.caldarelli@gmail.com>
+ * @since 3.0
+ */
+class ActiveQueryEvent extends Event
+{
+    /**
+     * @event raised after executing init 
+     */    
+    const INIT = 'yii\base\Event\ActiveQueryEvent::INIT';
+
+    /**
+     * Creates INIT event.
+     * @return self created event
+     */
+    public static function init(): self
+    {
+        return new static(static::INIT);
+    }    
+
+}
