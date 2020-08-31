8af2f9cd3297402ecddb30bf273374dcfe6ece8a
Alexander Makarov <sam@rmcreative.ru>
2020-08-31 20:53:13 +0300

    Adjust to yiisoft/strings changes

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -455,7 +455,7 @@ class ActiveRecord extends BaseActiveRecord
     /**
      * Declares the name of the database table associated with this AR class.
      *
-     * By default this method returns the class name as the table name by calling {@see Inflector::camel2id()} with
+     * By default this method returns the class name as the table name by calling {@see Inflector::pascalCaseToId()} with
      * prefix {@see Connection::tablePrefix}. For example if {@see Connection::tablePrefix} is `tbl_`, `Customer`
      * becomes `tbl_customer`, and `OrderItem` becomes `tbl_order_item`. You may override this method if the table is
      * not named after this convention.
@@ -466,7 +466,7 @@ class ActiveRecord extends BaseActiveRecord
     {
         $inflector = new Inflector();
 
-        return '{{%' . $inflector->camel2id(StringHelper::basename(static::class), '_') . '}}';
+        return '{{%' . $inflector->pascalCaseToId(StringHelper::baseName(static::class), '_') . '}}';
     }
 
     /**
