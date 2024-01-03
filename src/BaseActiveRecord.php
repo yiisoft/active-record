ff9f10109f0c89180a5f6fe25dcaee22bccf16b1
行风 <niqy@qq.com>
2024-01-03 10:13:25 +0800

    Update src/BaseActiveRecord.php
    Co-authored-by: Sergei Tigrov <rrr-r@ya.ru>

--- src/BaseActiveRecord.php
+++ src/BaseActiveRecord.php
@@ -327,8 +327,7 @@ abstract class BaseActiveRecord implements ActiveRecordInterface, IteratorAggreg
      *
      * @return ActiveQueryInterface The relational query object.
      *
-     * @psalm-template T of ActiveRecordInterface
-     * @psalm-param class-string<T> $arClass
+     * @psalm-param class-string<ActiveRecordInterface> $arClass
      */
     public function hasMany(string $class, array $link): ActiveQueryInterface
     {
