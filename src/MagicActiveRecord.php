b482833bf07dc5ecdd44903c7e553961c61f3e11
Tigrov <rrr-r@ya.ru>
2024-05-21 18:15:57 +0700

    Add `MagicActiveRecord`

--- src/MagicActiveRecord.php
+++ src/MagicActiveRecord.php
@@ -9,6 +9,8 @@ use IteratorAggregate;
 use Yiisoft\ActiveRecord\Trait\ArrayableTrait;
 use Yiisoft\ActiveRecord\Trait\ArrayAccessTrait;
 use Yiisoft\ActiveRecord\Trait\ArrayIteratorTrait;
+use Yiisoft\ActiveRecord\Trait\MagicPropertiesTrait;
+use Yiisoft\ActiveRecord\Trait\MagicRelationsTrait;
 use Yiisoft\ActiveRecord\Trait\TransactionalTrait;
 use Yiisoft\Arrays\ArrayableInterface;
 
@@ -19,13 +21,15 @@ use Yiisoft\Arrays\ArrayableInterface;
  * @see ArrayAccess to access attributes as array elements;
  * @see IteratorAggregate to iterate over attributes;
  * @see TransactionalInterface to handle transactions;
+ * @see MagicPropertiesTrait to access attributes as properties;
+ * @see MagicRelationsTrait to access relation queries.
  *
  * @template-implements ArrayAccess<string, mixed>
  * @template-implements IteratorAggregate<string, mixed>
  *
  * @see BaseActiveRecord for more information.
  */
-class ActiveRecord extends BaseActiveRecord implements
+class MagicActiveRecord extends BaseActiveRecord implements
     ArrayableInterface,
     ArrayAccess,
     IteratorAggregate,
@@ -35,4 +39,6 @@ class ActiveRecord extends BaseActiveRecord implements
     use ArrayAccessTrait;
     use ArrayIteratorTrait;
     use TransactionalTrait;
+    use MagicPropertiesTrait;
+    use MagicRelationsTrait;
 }
