9b70a343400b81a48af5ae6cb6c55bcf57dd84d6
Tigrov <rrr-r@ya.ru>
2024-05-21 11:29:36 +0700

    Refactor `ArrayAccessTrait` for compatibility without magical implementation

--- src/Trait/ArrayAccessTrait.php
+++ src/Trait/ArrayAccessTrait.php
@@ -4,10 +4,36 @@ declare(strict_types=1);
 
 namespace Yiisoft\ActiveRecord\Trait;
 
+use InvalidArgumentException;
+use Yiisoft\ActiveRecord\ActiveRecordInterface;
+
+use function get_class;
+use function get_object_vars;
 use function property_exists;
 
 /**
  * Trait to implement {@see ArrayAccess} interface for ActiveRecord.
+ *
+ * @method mixed getAttribute(string $name)
+ * @see ActiveRecordInterface::getAttribute()
+ *
+ * @method bool hasAttribute(string $name)
+ * @see ActiveRecordInterface::hasAttribute()
+ *
+ * @method void setAttribute(string $name, mixed $value)
+ * @see ActiveRecordInterface::getAttribute()
+ *
+ * @method ActiveRecordInterface relation(string $name)
+ * @see ActiveRecordInterface::relation()
+ *
+ * @method bool isRelationPopulated(string $name)
+ * @see ActiveRecordInterface::isRelationPopulated()
+ *
+ * @method void populateRelation(string $name, ActiveRecordInterface $record)
+ * @see ActiveRecordInterface::populateRelation()
+ *
+ * @method void resetRelation(string $name)
+ * @see ActiveRecordInterface::resetRelation()
  */
 trait ArrayAccessTrait
 {
@@ -18,16 +44,41 @@ trait ArrayAccessTrait
      *
      * It is implicitly called when you use something like `isset($model[$offset])`.
      *
+     * @param string $offset the offset to check on.
+     *
      * @return bool whether or not an offset exists.
      */
     public function offsetExists(mixed $offset): bool
     {
-        return isset($this->$offset);
+        if ($this->hasAttribute($offset)) {
+            return $this->getAttribute($offset) !== null;
+        }
+
+        if (property_exists($this, $offset)) {
+            return isset(get_object_vars($this)[$offset]);
+        }
+
+        if ($this->isRelationPopulated($offset)) {
+            return $this->relation($offset) !== null;
+        }
+
+        return false;
     }
 
+    /**
+     * @param string $offset the offset to retrieve element.
+     */
     public function offsetGet(mixed $offset): mixed
     {
-        return $this->$offset;
+        if ($this->hasAttribute($offset)) {
+            return $this->getAttribute($offset);
+        }
+
+        if (property_exists($this, $offset)) {
+            return get_object_vars($this)[$offset] ?? null;
+        }
+
+        return $this->relation($offset);
     }
 
     /**
@@ -36,10 +87,28 @@ trait ArrayAccessTrait
      * This method is required by the SPL interface {@see ArrayAccess}.
      *
      * It is implicitly called when you use something like `$model[$offset] = $item;`.
+     *
+     * @param string $offset the offset to set element.
+     * @param mixed $value the element value.
      */
     public function offsetSet(mixed $offset, mixed $value): void
     {
-        $this->$offset = $value;
+        if ($this->hasAttribute($offset)) {
+            $this->setAttribute($offset, $value);
+            return;
+        }
+
+        if (property_exists($this, $offset)) {
+            $this->$offset = $value;
+            return;
+        }
+
+        if ($value instanceof ActiveRecordInterface) {
+            $this->populateRelation($offset, $value);
+            return;
+        }
+
+        throw new InvalidArgumentException('Setting unknown property: ' . get_class($this) . '::' . $offset);
     }
 
     /**
@@ -48,13 +117,21 @@ trait ArrayAccessTrait
      * This method is required by the SPL interface {@see ArrayAccess}.
      *
      * It is implicitly called when you use something like `unset($model[$offset])`.
+     *
+     * @param string $offset the offset to unset element
      */
     public function offsetUnset(mixed $offset): void
     {
-        if (is_string($offset) && property_exists($this, $offset)) {
-            $this->$offset = null;
-        } else {
-            unset($this->$offset);
+        if ($this->hasAttribute($offset)) {
+            $this->setAttribute($offset, null);
+            return;
         }
+
+        if (property_exists($this, $offset)) {
+            $this->$offset = null;
+            return;
+        }
+
+        $this->resetRelation($offset);
     }
 }
