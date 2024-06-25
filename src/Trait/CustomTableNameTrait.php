2e04d0f91e5470ce2d04433a951a8230f1beb00f
Tigrov <rrr-r@ya.ru>
2024-06-25 14:25:35 +0700

    Add comments to trait [skip ci]

--- src/Trait/CustomTableNameTrait.php
+++ src/Trait/CustomTableNameTrait.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Yiisoft\ActiveRecord\Trait;
 
+/**
+ * Trait to implement custom table name for ActiveRecord.
+ *
+ * @see ActiveRecordInterface::getTableName()
+ */
 trait CustomTableNameTrait
 {
     private string $tableName;
 
+    /**
+     * Sets the table name for the ActiveRecord.
+     */
     public function withTableName($tableName): static
     {
         $new = clone $this;
