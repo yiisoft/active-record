272d393da0c6f1d50b069e783c0770ad2c090118
Tigrov <rrr-r@ya.ru>
2025-05-09 11:19:12 +0700

    Improve test coverage

--- tests/ActiveQueryTest.php
+++ tests/ActiveQueryTest.php
@@ -1922,8 +1922,19 @@ abstract class ActiveQueryTest extends TestCase
         $this->assertSame(1, $order->update(['total' => $newTotal]));
         $this->assertEquals($newTotal, $order->getTotal());
 
-        $order = $orderQuery->findByPk(1);
+        $order->refresh();
         $this->assertEquals($newTotal, $order->getTotal());
+
+        // update only one property
+        $this->assertSame(1, $order->getCustomerId());
+
+        $order->set('total', 1000);
+        $order->set('customer_id', 2);
+
+        $this->assertSame(1, $order->update(['total']));
+        $order->refresh();
+        $this->assertEquals(1000, $order->getTotal());
+        $this->assertSame(1, $order->getCustomerId());
 
         /** @see https://github.com/yiisoft/yii2/issues/12143 */
         $newOrder = new Order();
