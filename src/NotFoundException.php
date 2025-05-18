921bb4d2f9686f3b7b7877d46b16c914a8a434eb
Sergei Tigrov <rrr-r@ya.ru>
2025-05-18 09:01:05 +0700

    Add find or fail methods (#421)

---
+++ src/NotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\ActiveRecord;
+
+use Yiisoft\Db\Exception\Exception;
+
+/**
+ * Represents an exception thrown when a record is not found.
+ */
+final class NotFoundException extends Exception
+{
+}
