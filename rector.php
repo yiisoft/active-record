a7f329493754a8da7f48d30f6a08873e53b8bf34
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-06-20 10:35:05 +0300

    Improve rector CI (#432)

--- rector.php
+++ rector.php
@@ -6,25 +6,16 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
-        /** Disable rector on tests */
-        // __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
-        NullToStrictStringFuncCallArgRector::class,
+    ])
+    ->withPhpSets(php81: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         ReadOnlyPropertyRector::class,
+        NullToStrictStringFuncCallArgRector::class,
     ]);
-};
