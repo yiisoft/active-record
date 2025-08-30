6dab78e7695584360063d4182f5c3b2c48d1f3a2
Tigrov <rrr-r@ya.ru>
2025-08-30 12:58:50 +0700

    Rearrange expression namespaces

--- tests/Driver/Pgsql/ActiveRecordTest.php
+++ tests/Driver/Pgsql/ActiveRecordTest.php
@@ -16,9 +16,9 @@ use Yiisoft\ActiveRecord\Tests\Stubs\ActiveRecord\DefaultPk;
 use Yiisoft\ActiveRecord\Tests\Stubs\ActiveRecord\UserAR;
 use Yiisoft\ActiveRecord\Tests\Support\PgsqlHelper;
 use Yiisoft\Db\Connection\ConnectionInterface;
-use Yiisoft\Db\Expression\ArrayExpression;
+use Yiisoft\Db\Expression\Value\ArrayExpression;
 use Yiisoft\Db\Expression\Expression;
-use Yiisoft\Db\Expression\JsonExpression;
+use Yiisoft\Db\Expression\Value\JsonExpression;
 use Yiisoft\Db\Pgsql\Schema as SchemaPgsql;
 use Yiisoft\Factory\Factory;
 
