2a297a8ec6fa79c7fd2145e760bfec65509ea9f4
Tigrov <rrr-r@ya.ru>
2025-09-03 16:12:21 +0700

    Add `UnknownPropertyException` exception

---
+++ src/UnknownPropertyException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\ActiveRecord;
+
+use Yiisoft\Db\Exception\Exception;
+
+/**
+ * Represents an exception caused by accessing unknown object properties.
+ */
+final class UnknownPropertyException extends Exception
+{
+}
--- tests/ActiveQueryTest.php
+++ tests/ActiveQueryTest.php
@@ -27,7 +27,7 @@ use Yiisoft\Db\Exception\Exception;
 use InvalidArgumentException;
 use Yiisoft\Db\Exception\InvalidCallException;
 use Yiisoft\Db\Exception\InvalidConfigException;
-use Yiisoft\Db\Exception\UnknownPropertyException;
+use Yiisoft\ActiveRecord\UnknownPropertyException;
 use Yiisoft\Db\Query\QueryInterface;
 
 use function sort;
