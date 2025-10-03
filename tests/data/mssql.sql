aeaa6f924d9937d22adb57b6e822c927a062c118
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-10-03 12:25:29 +0300

    fix oci

--- tests/data/mssql.sql
+++ tests/data/mssql.sql
@@ -423,3 +423,4 @@ CREATE TABLE [tbl_user]
 SET IDENTITY_INSERT [tbl_user] ON;
 INSERT INTO [tbl_user] (id, name) VALUES (1, 'Sergei');
 INSERT INTO [tbl_user] (id, name) VALUES (2, null);
+SET IDENTITY_INSERT [tbl_user] OFF;
