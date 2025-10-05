878e12cfc70153ff634f8286d02e3e857ec1a371
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-10-05 21:30:25 +0300

    4

---
+++ tests/Stubs/ActiveRecord/CustomerArrayAccessModel.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\ActiveRecord\Tests\Stubs\ActiveRecord;
+
+use ArrayAccess;
+use Yiisoft\ActiveRecord\ActiveQuery;
+use Yiisoft\ActiveRecord\ActiveRecordInterface;
+use Yiisoft\ActiveRecord\Tests\Stubs\ArrayableActiveRecord;
+use Yiisoft\ActiveRecord\Trait\ArrayAccessTrait;
+
+final class CustomerArrayAccessModel extends ArrayableActiveRecord implements ArrayAccess
+{
+    use ArrayAccessTrait;
+
+    public ?int $id = null;
+    public ?string $name = null;
+    public ?string $email = null;
+    public ?int $profile_id = null;
+    public mixed $customProperty = null;
+
+    public function tableName(): string
+    {
+        return 'customer';
+    }
+
+    public function relationQuery(string $name): ActiveQuery
+    {
+        return match ($name) {
+            'profile' => $this->hasOne(Profile::class, ['id' => 'profile_id']),
+            'orders' => $this->hasMany(Order::class, ['customer_id' => 'id']),
+            default => parent::relationQuery($name),
+        };
+    }
+}
