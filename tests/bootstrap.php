6226e90b301f132ba6b18fe155a79ed3b486ce1e
Tigrov <rrr-r@ya.ru>
2025-10-12 19:48:15 +0700

    fix environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-if (getenv('ENVIRONMENT', local_only: true) !== 'ci') {
+if (getenv('ENVIRONMENT', local_only: true) === 'local') {
     $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
     $dotenv->load();
 }
