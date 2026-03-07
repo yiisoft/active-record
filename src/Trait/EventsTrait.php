bfbd15bad0fa94da7c0c9fd49c2a122fb9158463
Mr. 42 <admin@mr42.me>
2026-03-07 09:13:51 +0000

    No constructor from Trait

--- src/Trait/EventsTrait.php
+++ src/Trait/EventsTrait.php
@@ -86,7 +86,7 @@ trait EventsTrait
     {
         $model = $modelClass instanceof ActiveRecordInterface
             ? $modelClass
-            : (new ReflectionClass($modelClass ?? static::class))->newInstance();
+            : (new ReflectionClass($modelClass ?? static::class))->newInstanceWithoutConstructor();
 
         $eventDispatcher = EventDispatcherProvider::get($model::class);
         $eventDispatcher->dispatch($event = new BeforeCreateQuery($model));
