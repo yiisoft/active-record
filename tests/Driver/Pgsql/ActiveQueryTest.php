18874624eba3d120ac85d369e48a166728660381
Dmitry Bukhonov <dmitrybuhonov@gmail.com>
2026-03-17 18:51:56 +0400

    Add test for ModelRelationFilter handling array column conditions

--- tests/Driver/Pgsql/ActiveQueryTest.php
+++ tests/Driver/Pgsql/ActiveQueryTest.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\ActiveRecord\Tests\Driver\Pgsql;
 
+use Yiisoft\ActiveRecord\Internal\ModelRelationFilter;
+use Yiisoft\ActiveRecord\Tests\Driver\Pgsql\Stubs\Promotion;
 use Yiisoft\ActiveRecord\Tests\Stubs\ActiveRecord\BitValues;
 use Yiisoft\ActiveRecord\Tests\Support\PgsqlHelper;
 use Yiisoft\Db\Connection\ConnectionInterface;
+use Yiisoft\Db\Expression\Value\ArrayValue;
+use Yiisoft\Db\QueryBuilder\Condition\ArrayOverlaps;
 
 final class ActiveQueryTest extends \Yiisoft\ActiveRecord\Tests\ActiveQueryTest
 {
@@ -19,6 +23,22 @@ final class ActiveQueryTest extends \Yiisoft\ActiveRecord\Tests\ActiveQueryTest
         $bitValueQuery = BitValues::query();
         $trueBit = $bitValueQuery->findByPk(2);
         $this->assertSame(1, $trueBit->val);
+    }
+
+    public function testModelRelationFilterUsesArrayOverlapsForArrayColumns(): void
+    {
+        $query = Promotion::query()->link(['array_item_ids' => 'id']);
+
+        ModelRelationFilter::apply($query, [
+            ['id' => 1],
+        ]);
+
+        $where = $query->getWhere();
+
+        $this->assertInstanceOf(ArrayOverlaps::class, $where);
+        $this->assertSame('array_item_ids', $where->column);
+        $this->assertInstanceOf(ArrayValue::class, $where->values);
+        $this->assertSame([1], $where->values->value);
     }
 
     protected static function createConnection(): ConnectionInterface
