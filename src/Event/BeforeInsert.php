a8d8d19f1aeed0eecab9aa0c4fd952ab3ea6351b
Sergei Tigrov <rrr-r@ya.ru>
2026-05-03 14:38:02 +0700

    Update doc for insert, update and save methods (#569)

--- src/Event/BeforeInsert.php
+++ src/Event/BeforeInsert.php
@@ -8,7 +8,7 @@ use Yiisoft\ActiveRecord\ActiveRecordInterface;
 
 /**
  * Event triggered before a new record is inserted into the database.
- * It allows to modify properties that will be used for {@see ActiveRecordInterface::insert()} operation.
+ * It allows modifying properties that will be used for {@see ActiveRecordInterface::insert()} operation.
  *
  * @see ActiveRecordInterface::insert()
  */
@@ -16,7 +16,9 @@ final class BeforeInsert extends AbstractEvent
 {
     /**
      * @param ActiveRecordInterface $model The model that is being inserted.
-     * @param array|null &$properties The properties that will be used for the insert operation.
+     * @param array|null &$properties List of property names or name-values pairs that need to be inserted.
+     * If name-value pairs are specified, the values will be used for insertion.
+     * If `null`, the properties will be taken from the model.
      */
     public function __construct(ActiveRecordInterface $model, public ?array &$properties)
     {
